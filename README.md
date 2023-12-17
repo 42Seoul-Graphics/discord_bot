45cfeebec621d738815051acbf9b42ce41c28db6
bentraxgel <dhrtpdnjs@gmail.com>
2023-12-17 16:48:40 +0900

    wip

---
+++ README.md
@@ -0,0 +1,2 @@
+[ ] pip3 install -r requirement.txt
+[ ] 실행: python3 main.py
---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.environ.get("DISCORD_TOKEN")
+CHANNEL_ID = os.environ.get("CHANNEL_ID")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        channel = self.get_channel(int(CHANNEL_ID))
+        #  TODO: 조건에 따른 메시지 작성
+        text = 'someting ~~~'
+        print ("hello")
+        # await channel.send(text)
+        await self.close()
+    
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(TOKEN)
