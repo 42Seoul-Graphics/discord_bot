9051613e9150ab887dc0693316d7c45b64874680
yeonseong <yeonseong.dev@gmail.com>
2023-12-24 03:16:35 +0900

    feat: 2주마다 cron돌아가게 설정

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ import text_util
 import datetime
 
 # 짝수주에는 확인하지 않음
-# if datetime.datetime.now().isocalendar()[1] % 2 == 0:
-# 	exit(0)
+if datetime.datetime.now().isocalendar()[1] % 2 == 0:
+	exit(0)
 
 load_dotenv()
 
@@ -17,7 +17,6 @@ CHANNEL_ID = os.environ.get("CHANNEL_ID")
 class MyClient(discord.Client):
     async def on_ready(self):
         channel = self.get_channel(int(CHANNEL_ID))
-        #  TODO: 조건에 따른 메시지 작성
         uncompleted_members = text_util.get_uncompleted_member()
         await channel.send(f"이번회차에는 {uncompleted_members}님이 안했어요. 다음에는 꼭 해봐요!")
         await self.close()
